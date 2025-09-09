d1394cc6c8e4277a4055869a5e5d22c53895d381
wardr19 <wardr19@gmatc.matc.edu>
2025-09-09 13:36:02 -0500

    Completed activity 2

--- Application/obj/Debug/net9.0/Application.AssemblyInfo.cs
+++ Application/obj/Debug/net9.0/Application.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Application")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+27ab3d4225597b2a8a1aaac3e7bed872201c063a")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d9506ac53d8782382e82bc31d5002d26d7b08500")]
 [assembly: System.Reflection.AssemblyProductAttribute("Application")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Application")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
